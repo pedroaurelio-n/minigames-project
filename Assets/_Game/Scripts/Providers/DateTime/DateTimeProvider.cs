7c4258478b7ffd4f9e356f3671ac80152bda84d5
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2025-06-21 00:53:24 -0300

    feat: create datetime provider

---
+++ Assets/_Game/Scripts/Providers/DateTime/DateTimeProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime Now => DateTime.Now;
+    public DateTime UtcNow => DateTime.UtcNow;
+    public DateTime Today => DateTime.Today;
+    public long UnixTimeSeconds => DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+    public long UnixTimeMilliseconds => DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+    
+    public DateTime FromUnixTimeSeconds(long seconds) => DateTimeOffset.FromUnixTimeSeconds(seconds).UtcDateTime;
+
+    public DateTime FromUnixTimeMilliseconds (long milliseconds) =>
+        DateTimeOffset.FromUnixTimeMilliseconds(milliseconds).UtcDateTime;
+}
---
+++ Assets/_Game/Scripts/Providers/DateTime/IDateTimeProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public interface IDateTimeProvider
+{
+    DateTime Now { get; }
+    DateTime UtcNow { get; }
+    DateTime Today { get; }
+    long UnixTimeSeconds { get; }
+    long UnixTimeMilliseconds { get; }
+    
+    DateTime FromUnixTimeSeconds(long seconds);
+    DateTime FromUnixTimeMilliseconds(long milliseconds);
+}
